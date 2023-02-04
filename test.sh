858c61c7159da507826797df3283330cbcb5fc24
Paul LaMendola <paulguy119@gmail.com>
2023-02-03 23:28:48 -0500

    Got scrolling in any direction working.
    Unfortunately, it's too slow to be practical!

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ export WINEPREFIX=/home/paul/mk-90/mk90emex_wine
 
 cp smp0.bin ../mk90emex
 cd ../mk90emex
-gamescope -n -W 1580 -H 624 wine MK90.exe
+gamescope -n -W 1580 -H 624 --force-orientation right wine MK90.exe
